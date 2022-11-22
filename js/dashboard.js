00db12e1d360c5522e6f117c8d7ff29f39599063
Mohammad H. Maazallahi <mohammadhoseinmazalahi@gmail.com>
2022-11-22 05:51:19 +0330

    Initial commit

---
+++ js/dashboard.js
@@ -0,0 +1,98 @@
+const chart1 = document.getElementById('chart1');
+const chart2 = document.getElementById('chart2');
+
+let percentage_distribution = {}
+let entities = []
+let last_entity = []
+
+const PATH = '/api/';
+
+function getUUID() {
+    // "uuid" get parameter
+    let urlParams = new URLSearchParams(window.location.search);
+    return urlParams.get('uuid');
+}
+
+async function getEntitiesData() {
+    await axios.get(PATH + '/entities/' + getUUID()).then(function (response) {
+        entities = response.data;
+
+        const labels = []
+        const progress_data = {
+            labels: labels,
+            datasets: [{
+                label: 'Progress Over Time',
+                data: [],
+                fill: false,
+                borderColor: 'rgb(75, 192, 192)',
+                tension: 0.1
+            }]
+        };
+
+        // Loop through entities
+        for (let i = 0; i < entities.length; i++) {
+            let entity = entities[i]
+            labels.push(entity.date_created);
+            progress_data['datasets'][0]['data'].push(entity.percentage)
+        }
+
+        new Chart(chart2, {
+            type: 'line',
+            data: progress_data,
+        })
+    })
+        .catch(function (error) {
+            console.log(error);
+        })
+}
+
+function getLastEntityData() {
+    let entity_id = entities[entities.length - 1].id
+    axios.get(PATH + '/entities/' + getUUID() + '/' + entity_id).then(function (response) {
+        last_entity = response.data;
+        console.log(last_entity);
+        // Loop through last_entity's segments
+        for (let i = 0; i < last_entity.segments.length; i++) {
+            let segment = last_entity.segments[i]
+            if(percentage_distribution[segment.category_title] === undefined) {
+                percentage_distribution[segment.category_title] = segment.area
+            } else {
+                percentage_distribution[segment.category_title] += segment.area
+            }
+        }
+        let percentage_data = {
+            datasets: [{
+                // Values of percentage_distribution
+                data: Object.values(percentage_distribution),
+            }],
+
+            // These labels appear in the legend and in the tooltips when hovering different arcs
+            labels: Object.keys(percentage_distribution)
+        };
+        new Chart(chart1, {
+            type: 'doughnut',
+            data: percentage_data,
+        })
+
+        let title = document.getElementById('title');
+        let subtitle = document.getElementById('subtitle');
+        let progress = document.getElementById('progress');
+        let image = document.getElementById('latest-photo');
+
+        title.innerHTML = last_entity.percentage + '%';
+        progress.value = last_entity.percentage;
+        subtitle.innerHTML = "The most identifiable entity is " + last_entity.most_significant_detection + " with " + last_entity.most_significant_area;
+        image.src = last_entity.image
+    })
+
+        .catch(function (error) {
+            console.log(error);
+        })
+}
+
+getEntitiesData().then(() => {
+    getLastEntityData()
+});
+
+document.getElementById('return-link').href = "index.html?uuid=" + getUUID();
+
---
+++ js/index.js
@@ -0,0 +1,99 @@
+navigator.getUserMedia =
+    navigator.getUserMedia ||
+    navigator.webkitGetUserMedia ||
+    navigator.mozGetUserMedia;
+
+const PATH='/api/';
+
+navigator.getUserMedia(
+    {
+        video: {
+            facingMode: "environment"
+        }
+    },
+    function (stream) {
+        var video = document.querySelector("video");
+        video.srcObject = stream;
+        video.onloadedmetadata = function (e) {
+            video.play();
+        };
+    },
+    function (err) {
+        console.log("The following error occurred: " + err.name);
+    }
+);
+let uuid = '';
+const existing_only = document.getElementById("existing-only");
+
+function getUUID() {
+    // check if uuid cookie exists
+    if (document.cookie.split(';').filter((item) => item.includes('uuid=')).length) {
+        uuid = document.cookie.split('; ').find(row => row.startsWith('uuid')).split('=')[1];
+    }
+    // "uuid" get parameter
+    let urlParams = new URLSearchParams(window.location.search);
+    // Check if uuid is in url
+    if (urlParams.has("uuid")) {
+        uuid = urlParams.get('uuid');
+        refreshDashboardLink();
+        existing_only.style.display = 'inline';
+    }
+    else {
+        axios.get(PATH + '/uuid').then(function (response) {
+            uuid = response.data;
+            // Set uuid cookie with indefinite expiry
+            document.cookie = "uuid=" + uuid + "; expires=Fri, 31 Dec 9999 23:59:59 GMT";
+            refreshDashboardLink()
+
+        }).catch(function (error) {
+            alert("Failed to obtain UUID, Data cannot be saved.");
+        })
+    }
+}
+
+function refreshDashboardLink()
+{
+    const link_to_dashboard = document.getElementById("link-to-dashboard");
+    link_to_dashboard.href = link_to_dashboard.href + "?uuid=" + uuid;
+}
+function capturePhoto() {
+    const canvas = document.getElementById("canvas");
+    const top_text = document.getElementById("top-text");
+    const variable_text = document.getElementById("variable-text");
+    const context = canvas.getContext("2d");
+    const video = document.querySelector("video");
+
+    [canvas.width, canvas.height] = [video.videoWidth, video.videoHeight];
+    context.drawImage(video, 0, 0, video.videoWidth, video.videoHeight);
+    variable_text.innerHTML = "Photo taken. Sending Photo...";
+
+    let percentage = null;
+    const dataURL = canvas.toDataURL("image/jpeg", 1.0);
+    axios.get(PATH + '/entities/' + uuid + '/check')
+        .then(function (response) {
+            percentage = null;
+            send_request(dataURL, percentage);
+        })
+        .catch(function (error) {
+            // percentage = prompt("What percentage of the image is covered by the wall?", "10");
+            percentage = null;
+            send_request(dataURL, percentage)
+        })
+}
+
+function send_request(dataURL, percentage)
+{
+    const variable_text = document.getElementById("variable-text");
+    axios.post(PATH, {
+        uuid: uuid,
+        image: dataURL,
+        percentage: percentage,
+    }).then(function () {
+        variable_text.innerHTML = "Photo sent.";
+        existing_only.style.display = 'inline';
+    }).catch(function () {
+        variable_text.innerHTML = "Photo failed to send.";
+    });
+}
+
+getUUID()
