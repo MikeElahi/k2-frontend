4e6e1fad7ffe7dd82674e4101f2adc1edfa054da
Mohammad H. Maazallahi <mohammadhoseinmazalahi@gmail.com>
2022-11-22 06:57:35 +0330

    fix: add actual function to camera reset :D

--- js/index.js
+++ js/index.js
@@ -95,5 +95,38 @@ function send_request(dataURL, percentage)
         variable_text.innerHTML = "Photo failed to send.";
     });
 }
+function removeURLParameter(url, parameter) {
+    //prefer to use l.search if you have a location/link object
+    var urlparts = url.split('?');
+    if (urlparts.length >= 2) {
+
+        var prefix = encodeURIComponent(parameter) + '=';
+        var pars = urlparts[1].split(/[&;]/g);
+
+        //reverse iteration as may be destructive
+        for (var i = pars.length; i-- > 0;) {
+            //idiom for string.startsWith
+            if (pars[i].lastIndexOf(prefix, 0) !== -1) {
+                pars.splice(i, 1);
+            }
+        }
+
+        return urlparts[0] + (pars.length > 0 ? '?' + pars.join('&') : '');
+    }
+    return url;
+}
+
+function eraseCookie(name) {
+    document.cookie = name+'=; Max-Age=-99999999;';
+}
+
+function reset()
+{
+    uuid = '';
+    eraseCookie('uuid');
+    window.location.href = removeURLParameter(window.location.href, 'uuid');
+    // Refresh
+    window.location.reload();
+}
 
 getUUID()
